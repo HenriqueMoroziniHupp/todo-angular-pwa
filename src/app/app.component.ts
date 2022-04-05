6574a0d111883886594105967dba731d56a50589
Henrique Morozini Hupp <henriquehupp@hotmail.com>
2022-04-05 19:57:05 -0300

    add PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.models';
 
 @Component({
@@ -8,23 +9,69 @@ import { Todo } from 'src/models/todo.models';
 })
 export class AppComponent {
   public todos: Todo[] = [];
-  public title: String = 'Meu dia';
+  public title: String = 'T O D O';
+  public form: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
     // Será chamado todo vez que o componente inicia
-    this.todos.push(new Todo(1, 'Passear com o cachorro', true));
-    this.todos.push(new Todo(2, 'Lavar a louça', true));
-    this.todos.push(new Todo(3, 'Ir na praia', false));
-
+    this.form = this.formBuilder.group({
+      title: [
+        '',
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(56),
+          Validators.required,
+        ]),
+      ],
+    });
+    this.load();
+    console.log(this.todos);
   }
-  
-  public op: boolean = false;
-  alterarTexto() {
-    if (this.op) {
-      this.title = 'Porra mano';
-    } else {
-      this.title = 'Meu dia';
+
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear(); // Chamando a função clear para limpar o formulário
+  }
+
+  clear() {
+    this.form.reset();
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) this.todos.splice(index, 1);
+    this.save();
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+    this.save();
+  }
+
+  finished() {
+    let count = 0;
+    for (let index = 0; index < this.todos.length; index++) {
+      if (this.todos[index].done === false) count++;
     }
-    this.op = !this.op
+    return count
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todosLocalStorage', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todosLocalStorage');
+    // if (data !== null) this.todos = JSON.parse(data);
+    data !== null ? (this.todos = JSON.parse(data)) : (this.todos = []);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,39 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
   providers: [],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+  ],
+  // imports: [BrowserModule, ReactiveFormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+/**
+ * 
+ *   imports: [BrowserModule, ReactiveFormsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
+ */
